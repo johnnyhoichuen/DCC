8b273e466c01478deffb4403a1f7ed6b57d33163
johnnyhoichuen <hcchengaa@ust.hk>
2022-07-26 11:51:50 +0800

    model saver & threading test

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import time
 import torch
 import numpy as np
 import ray
-from worker import GlobalBuffer, Learner, Actor
+from worker import GlobalBuffer, Learner, Actor, ModelSaver
 import config
 
 os.environ["OMP_NUM_THREADS"] = "1"
@@ -16,11 +16,12 @@ random.seed(0)
 
 def main(num_actors=config.num_actors, log_interval=config.log_interval):
 
-    # ray.init()
-    ray.init(address=os.environ["ip_head"])
+    ray.init()
+    # ray.init(address=os.environ["ip_head"])
 
     buffer = GlobalBuffer.remote()
-    learner = Learner.remote(buffer)
+    model_saver = ModelSaver.remote()
+    learner = Learner.remote(buffer, model_saver)
     time.sleep(1)
     actors = [Actor.remote(i, 0.4**(1+(i/(num_actors-1))*7), learner, buffer) for i in range(num_cpus)]
 
@@ -38,8 +39,8 @@ def main(num_actors=config.num_actors, log_interval=config.log_interval):
 
     print('start training')
     buffer.run.remote()
-    learner.run.remote()
-    
+    ray.get(learner.run.remote())
+
     done = False
     while not done:
         time.sleep(log_interval)
@@ -52,4 +53,11 @@ if __name__ == '__main__':
     # address = sys.argv[2]
     print(f'train.py args: {sys.argv}')
 
-    main(num_actors=num_cpus)
+    config.num_actors = num_cpus
+    config.training_steps = round(2400000/config.num_actors)
+    # config.save_interval = round(config.training_steps * 0.05)
+    config.save_interval = 300
+
+    print(f'updated save_interval: {config.save_interval}, training_steps: {config.training_steps}')
+
+    main(num_actors=config.num_actors-8)
